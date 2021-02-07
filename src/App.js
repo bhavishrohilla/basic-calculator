935ff2d086c1c61d3ea044dadb856f0df7513506
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-02-07 17:41:36 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -25,28 +25,28 @@ class App extends Component {
       <div className="App">
         <div className="calc-wrapper">
           <div className="row">
-            <Input></Input>
+            <Input>{this.state.input}</Input>
           </div>
           <div className="row">
             <Button handleClick={this.addToInput}>7</Button>
-            <Button>8</Button>
-            <Button>9</Button>
+            <Button handleClick={this.addToInput}>8</Button>
+            <Button handleClick={this.addToInput}>9</Button>
             <Button>/</Button>
           </div>
           <div className="row">
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
+            <Button handleClick={this.addToInput}>4</Button>
+            <Button handleClick={this.addToInput}>5</Button>
+            <Button handleClick={this.addToInput}>6</Button>
             <Button>*</Button>
           </div>
           <div className="row">
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-            <Button>+</Button>
+            <Button handleClick={this.addToInput}>1</Button>
+            <Button handleClick={this.addToInput}>2</Button>
+            <Button handleClick={this.addToInput}>3</Button>
+            <Button handleClick={this.addToInput}>+</Button>
           </div>
           <div className="row">
-            <Button>.</Button>
+            <Button handleClick={this.addToInput}>.</Button>
             <Button>0</Button>
             <Button>=</Button>
             <Button>-</Button>
