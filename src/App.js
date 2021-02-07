483dacd95ad4b86b6c2064aa3494c22944c05aa8
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-02-07 15:58:28 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-import Button from './components/Button'
+import Button from './components/Button';
+import Input from './components/Input'
 function App() {
   return (
     <div className="App">
       <div className="calc-wrapper">
+        <div className="row">
+          <Input></Input>
+        </div>
         <div className="row">
           <Button>7</Button>
           <Button>8</Button>
---
+++ src/components/Input.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './Input.css';
+
+class Input extends Component {
+
+    render() {
+        return (
+            <div className="input">   
+            </div>
+        )
+    }
+};
+
+export default Input;
