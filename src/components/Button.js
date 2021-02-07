4f47bb7c12072762bf61505f1c575cc659c422b1
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-02-07 15:46:51 +0530

    operators

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import './Button.css';
 
 class Button extends Component {
+    isOperator = val => {
+        return !isNaN(val) || val === "." || val === "=";
+    }
+
     render() {
         return (
-        <div className="button">
+        <div className={`button ${this.isOperator(this.props.children) ? "" : "operator"}`}>
             {this.props.children}
         </div>
-        );
+        )
     }
 };
 
