19c214200856e4c77a736f4c5bc4e3673a4c363c
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2021-02-14 14:28:54 +0530

    updated file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Button from './components/Button';
 import ClearButton from './components/ClearButton';
 import Input from './components/Input';
+
+
 class App extends Component {
 
   constructor(props) {
@@ -51,46 +53,49 @@ class App extends Component {
   evaluate = () => {
     this.state.currentNumber = this.state.input;
     if(this.state.operator == "plus"){ 
-      this.setState({ input : parseInt(this.state.previousNumber) + parseInt(this.state.currentNumber) });
+      this.setState({
+        input : parseInt(this.state.previousNumber) + parseInt(this.state.currentNumber)
+      });
+    }
   };
 
   render() {
-    return (
-      <div className="App">
-        <div className="calc-wrapper">
-          <div className="row">
-            <Input>{this.state.input}</Input>
-          </div>
-          <div className="row">
-            <Button handleClick={this.addToInput}>7</Button>
-            <Button handleClick={this.addToInput}>8</Button>
-            <Button handleClick={this.addToInput}>9</Button>
-            <Button>/</Button>
-          </div>
-          <div className="row">
-            <Button handleClick={this.addToInput}>4</Button>
-            <Button handleClick={this.addToInput}>5</Button>
-            <Button handleClick={this.addToInput}>6</Button>
-            <Button>*</Button>
-          </div>
-          <div className="row">
-            <Button handleClick={this.addToInput}>1</Button>
-            <Button handleClick={this.addToInput}>2</Button>
-            <Button handleClick={this.addToInput}>3</Button>
-            <Button handleClick={this.add}>+</Button>
-          </div>
-          <div className="row">
-            <Button handleClick={this.addTDecimal}>.</Button>
-            <Button handleClick={this.addZeroToInput}>0</Button>
-            <Button handleClick={this.evaluate}>=</Button>
-            <Button>-</Button>
-          </div>
-          <div className="row">
-            <ClearButton handleClear={this.clearInput}>Clear</ClearButton>
+      return (
+        <div className="App">
+          <div className="calc-wrapper">
+            <div className="row">
+              <Input>{this.state.input}</Input>
+            </div>
+            <div className="row">
+              <Button handleClick={this.addToInput}>7</Button>
+              <Button handleClick={this.addToInput}>8</Button>
+              <Button handleClick={this.addToInput}>9</Button>
+              <Button>/</Button>
+            </div>
+            <div className="row">
+              <Button handleClick={this.addToInput}>4</Button>
+              <Button handleClick={this.addToInput}>5</Button>
+              <Button handleClick={this.addToInput}>6</Button>
+              <Button>*</Button>
+            </div>
+            <div className="row">
+              <Button handleClick={this.addToInput}>1</Button>
+              <Button handleClick={this.addToInput}>2</Button>
+              <Button handleClick={this.addToInput}>3</Button>
+              <Button handleClick={this.add}>+</Button>
+            </div>
+            <div className="row">
+              <Button handleClick={this.addTDecimal}>.</Button>
+              <Button handleClick={this.addZeroToInput}>0</Button>
+              <Button handleClick={this.evaluate}>=</Button>
+              <Button>-</Button>
+            </div>
+            <div className="row">
+              <ClearButton handleClear={this.clearInput}>Clear</ClearButton>
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
   };
 };
 
